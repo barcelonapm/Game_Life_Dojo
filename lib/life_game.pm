1a6ce644c65895177ff282182c1464decd255fdf
Jose Luis Perez Diez <jluis@escomposlinux.org>
2016-06-08 14:35:57 +0200

    pases presentation test

--- lib/life_game.pm
+++ lib/life_game.pm
@@ -6,11 +6,28 @@ module life_game {
    sub resolve_board (Str $board) is export {
        my $result = $board;
        $result  ~~ s:g/<-[\d\.\,\*]>//;
-       my @estate = split(',',$result); 
-       return '' if @estate[0,1] ~~ /\D/;
-       return '' if chars(@estate[2]) < @estate[0] * @estate[1];
-       return $board;
+       my @state = split(',',$result); 
+       return '' if @state[0,1] ~~ /\D/;
+       return '' if chars(@state[2]) < @state[0] * @state[1];
+       return join(',',@state);
    }
+   sub print_board (Str $board) is export {
+       my $result = $board;
+       $result  ~~ s:g/<-[\d\.\,\*]>//;
+       my @state = split(',',$result); 
+       return '' if @state[0,1] ~~ /\D/;
+       return '' if chars(@state[2]) < @state[0] * @state[1];
+       my $head = "(@state[0],@state[1]),\n";
+       my $body ='';
+       my $row = 0;
+       while (++$row <= @state[0]) {
+          $body ~= "\n" if $body;
+          $body ~= ' 'x chars($head)-2;
+          $body ~= substr(@state[2],($row-1)*@state[1],@state[1]);
+       }
+       return $head ~ $body;
+    }
+
 }
 
 
