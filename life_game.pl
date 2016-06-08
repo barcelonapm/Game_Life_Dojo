3167117872915b8fe4411e487efdb5c347e64a4d
Jose Luis Perez Diez <jluis@escomposlinux.org>
2016-06-08 14:42:54 +0200

    play 5 rounds

--- life_game.pl
+++ life_game.pl
@@ -5,7 +5,15 @@ sub MAIN {
     use lib 'lib';
     use life_game;
     for lines() {
-        resolve_board($_).say ;
+        print_board($_).say;
+        my $next = resolve_board($_);
+        print_board($next).say ;
+        $next = resolve_board($next);
+        print_board($next).say ;
+        $next = resolve_board($next);
+        print_board($next).say ;
+        $next = resolve_board($next);
+        print_board($next).say ;
     }
 }
      
